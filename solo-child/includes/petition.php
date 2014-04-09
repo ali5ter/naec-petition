431d7ae3dae18e27e930e1e37c674db9f1b6cfc8
Alister Lewis-Bowen <alister@different.com>
2014-04-09 00:02:27 -0400

    add POST method process hook and fix default page bug on pager and book

--- solo-child/includes/petition.php
+++ solo-child/includes/petition.php
@@ -9,10 +9,10 @@ $_petition_pages = 1;
 $_petition_dateFormat = 'D j M Y';
 $_petition_timeFormat = 'g:i a';
 
-if ($_POST['petition']) petitionEntry();
+if ($_GET['petition']) petitionEntry();
 
 function petitionEntry() {
-    print('ARFARFARAFARAF');
+    # TODO: Process POST request
 }
 
 function petitionSigners() {
@@ -23,28 +23,28 @@ function petitionSigners() {
 function petitionPages() {
     global $_petition_name, $_petition_entries, $_petition_pages;
 
-    if(!$_SERVER['QUERY_STRING']) { $n = 1; }
+    if(empty($_GET['n'])) { $n = 1; }
     else { $n = $_GET['n']; }
 
     $d = $_petition_entries / 10;
     $f = floor($d);
     $_petition_pages = ($d == $f)? $f : $f + 1;
-    $anchor = '#'.$_petition_name.'_petition_entries';
+    $anchor = '#'.$_petition_name.'_petition';
 
-    if ($n > 1) { echo '<a href="?n='.($n - 1).$anchor.'">&#9669;</a> '; }
+    if ($n > 1) { echo '<a class="first" href="?n='.($n - 1).$anchor.'">&#9669;</a> '; }
 
     for ($i = 1; $i <= $_petition_pages; $i++) {
-        if ($i == $n) { echo $i.' '; }
+        if ($i == $n) { echo '<span class="selected">'.$i.'</span> '; }
         else { echo '<a href="?n='.$i.$anchor.'">'.$i.'</a> '; }
     }
 
-    if ($n < $_petition_pages) { echo '<a href="?n='.($n + 1).$anchor.'">&#9659;</a> '; }
+    if ($n < $_petition_pages) { echo '<a class="last" href="?n='.($n + 1).$anchor.'">&#9659;</a> '; }
 }
 
 function petitionBook() {
     global $_petition_entries_file_lines, $_petition_entries;
 
-    $n = (!$_SERVER['QUERY_STRING'])? 1 : $_GET['n'];
+    $n = (empty($_GET['n']))? 1 : $_GET['n'];
     $min = 10 * ($n - 1);
     $max = 10 * $n - 1;
 
@@ -58,18 +58,19 @@ function petitionBook() {
 }
 
 ?>
-<div class="petition inside clearfix">
-    <form method="post" action="<?php print $_SERVER['PHP_SELF']; ?>">
+<div id="<?php print $_petition_name; ?>_petition" class="petition inside clearfix">
+    <form method="post" action="?petition=sign#<?php print $_petition_name; ?>_petition">
         <input type="text"  name="signername" placeholder="Your name" autocomplete="off" tabindex="1">
         <input type="text" name="email" placeholder="Your email address" autocomplete="off" tabindex="2">
         <input type="text" name="address" placeholder="Your home address" autocomplete="off" tabindex="3">
         <textarea name="message" placeholder="Your comments" tabindex="5"></textarea>
+        <input type="hidden" name="petition" value="<?php print $_petition_name; ?>">
         <input type="submit" name="submit" tabindex="6" value="Sign the petition!">
     </form>
-    <div id="<?php print $_petition_name; ?>_petition_entries" class="scrollWrap">
-        <p class="pager"><strong><?php petitionSigners(); ?></strong> signatories.<br/>Page: <?php petitionPages() ?></p>
+    <div class="petition_entries_book">
+        <p class="pager"><strong><?php petitionSigners(); ?></strong> signatories.<br/><?php petitionPages() ?></p>
         <?php petitionBook() ?>
-        <p class="pager"><strong><?php petitionSigners(); ?></strong> signatories.<br/>Page: <?php petitionPages() ?></p>
+        <p class="pager"><strong><?php petitionSigners(); ?></strong> signatories.<br/><?php petitionPages() ?></p>
     </div>
 </div>
 <?php $wp_query = $temp_query; ?>
